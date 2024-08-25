6428c67d3d906abac42293815845dfb598d5182a
Lainel John Dela Cruz <ljbdelacruz123@gmail.com>
2024-08-25 12:14:18 +0800

    dependencies setup

---
+++ README.md
@@ -0,0 +1,9 @@
+# rzr_erp_weather_app
+
+### How to run
+
+```
+flutter pub get
+flutter run 
+```
+
---
+++ android/app/src/main/kotlin/com/example/rzr_erp_weather_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.rzr_erp_weather_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
